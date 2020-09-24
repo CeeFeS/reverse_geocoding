120bc7aa76b182bf8877554c5498e4453ad2d59a
cf_schulze <cf_schulze@web.de>
2020-09-24 13:08:12 +0200

    + input of UTM ZONE

--- reverse_geocoding_script.py
+++ reverse_geocoding_script.py
@@ -1,6 +1,9 @@
 from geopy.geocoders import Nominatim
 import utm
 
+print("UTM ZONE (https://mangomap.com/robertyoung/maps/69585/what-utm-zone-am-i-in-#): ")
+input_utm_zone = int(input())
+
 while True:
 
     #input as terminal tool
@@ -11,11 +14,11 @@ while True:
     # input longtude (y) UTM33
     input_y = float(input())
     # utm (UTM33) to DG
-    long_lat = utm.to_latlon(input_x, input_y, 33, 'U')
+    long_lat = utm.to_latlon(input_x, input_y, input_utm_zone, 'U')
     print(long_lat)
     # reverse geo engineer with DG
     geolocator = geolocator = Nominatim(user_agent="cfs_geo_reverse")
     location = geolocator.reverse(long_lat)
-    
+
     print(location.address)
     print("-----------------------------------")
