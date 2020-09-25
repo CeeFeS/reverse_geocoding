3c5d62a27839ae6e18728bc20de602f51173c81b
Carl -F. Schulze <mail@cf-schulze.de>
2020-09-25 08:55:57 +0200

    added username as user-agent

--- reverse_geocoding_script.py
+++ reverse_geocoding_script.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 import utm
+import getpass
 
 print("UTM ZONE (https://mangomap.com/robertyoung/maps/69585/what-utm-zone-am-i-in-#): ")
 input_utm_zone = int(input())
@@ -16,8 +17,10 @@ while True:
     # utm (UTM33) to DG
     long_lat = utm.to_latlon(input_x, input_y, input_utm_zone, 'U')
     print(long_lat)
+    #useragent is current username
+    username = getpass.getuser()
     # reverse geo engineer with DG
-    geolocator = geolocator = Nominatim(user_agent="cfs_geo_reverse")
+    geolocator = geolocator = Nominatim(user_agent=username)
     location = geolocator.reverse(long_lat)
 
     print(location.address)
