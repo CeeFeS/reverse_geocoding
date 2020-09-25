d389ad233fcc65b6eec74f6600253ec5e8226dad
Carl -F. Schulze <mail@cf-schulze.de>
2020-09-25 10:20:38 +0200

    improved visibility

--- reverse_geocoding_script.py
+++ reverse_geocoding_script.py
@@ -1,9 +1,21 @@
 from geopy.geocoders import Nominatim
 import utm
 import getpass
+# import only system from os
+from os import system, name
+
+# define our clear function
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 print("UTM ZONE (https://mangomap.com/robertyoung/maps/69585/what-utm-zone-am-i-in-#): ")
 input_utm_zone = int(input())
+clear()
 
 while True:
 
@@ -16,12 +28,13 @@ while True:
     input_y = float(input())
     # utm (UTM33) to DG
     long_lat = utm.to_latlon(input_x, input_y, input_utm_zone, 'U')
-    print(long_lat)
     #useragent is current username
     username = getpass.getuser()
     # reverse geo engineer with DG
     geolocator = geolocator = Nominatim(user_agent=username)
     location = geolocator.reverse(long_lat)
-
+    #print results
+    print("\n------results------\n")
+    print(long_lat)
     print(location.address)
-    print("-----------------------------------")
+    print("\n-------------------\n\n")
